a6a9bd20733d94c064224bab2c2483b59b442713
Chet Powers <ccpowers@bu.edu>
2017-03-25 18:36:46 -0400

    added jpegger

---
+++ jpegger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from PIL import Image
+import StringIO
+
+output = StringIO.StringIO()
+ 
+
+img = Image.open("file.jpg")
+img.save(output, format="JPEG")
+
+for i in range(100):
+    im = Image.open(output)
+    im.save(output, format="JPEG", quality=10)
+
+img_final = Image.open(output)
+img_final.save("file2.jpg", format="JPEG", quality=1)
