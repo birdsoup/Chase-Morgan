e932b3c1927c84114aeefae951d720fe967225c3
Chet Powers <ccpowers@bu.edu>
2017-03-25 18:38:39 -0400

    updated jpegger, made it a function

--- jpegger.py
+++ jpegger.py
@@ -2,15 +2,17 @@
 from PIL import Image
 import StringIO
 
-output = StringIO.StringIO()
+
+def jpegify(filename):
+    output = StringIO.StringIO()
  
 
-img = Image.open("file.jpg")
-img.save(output, format="JPEG")
+    img = Image.open(filename)
+    img.save(output, format="JPEG")
 
-for i in range(100):
-    im = Image.open(output)
-    im.save(output, format="JPEG", quality=10)
+    for i in range(100):
+        im = Image.open(output)
+        im.save(output, format="JPEG", quality=10)
 
-img_final = Image.open(output)
-img_final.save("file2.jpg", format="JPEG", quality=1)
+    img_final = Image.open(output)
+    img_final.save(filename + "_output", format="JPEG", quality=1)
