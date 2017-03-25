25ade29d52c0ba7f0b770a1135cf93a4f022580a
Chet Powers <ccpowers@bu.edu>
2017-03-25 14:50:14 -0400

    updated page

--- website/config.py
+++ website/config.py
@@ -10,6 +10,8 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db/db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = False
 
+#this shouldn't be in a public git repo, but it's not being hosted online
+#and this key isn't used anywhere else so it should be fine
 SECRET_KEY = '9\xe9\xbfAf\xf2\x11\xf7u M\x7f\x9f\xae\xa6\x16\ta\xfb\rT\x84@\x96'
--- website/forms.py
+++ website/forms.py
@@ -5,4 +5,4 @@ from wtforms.validators import DataRequired
 search_validator = validators.Regexp('^[a-zA-Z0-9\. ]*$', message="Search contains invalid characters.")
 
 class SearchForm(Form):
-    search_terms = StringField("search_terms", validators=[search_validator, validators.Length(max=100, message="Search too long."), DataRequired()])
+    keywords = StringField("keywords", validators=[search_validator, validators.Length(max=100, message="Search too long.")])
