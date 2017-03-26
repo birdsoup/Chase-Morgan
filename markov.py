52a95543ffc91c57392e801b2f669b70379f3230
Rudhra Raveendran <rudytheninja@gmail.com>
2017-03-25 21:08:17 -0400

    meme text generator example

--- markov.py
+++ markov.py
@@ -1,9 +1,15 @@
 import markovify
 
-def generateMemeText:
-	with open("./corpus.txt") as f:
-    text = f.read()
-	text_model = markovify.Text(text)
-	top = text_model.make_sentence()
-	bottom = text_model.make_sentence()
-	return [top, bottom]
+def generateMemeText(top, bottom):
+	with open("./joke_corpus.txt") as f:
+		text = f.read()
+		text_model = markovify.Text(text)
+		top = text_model.make_sentence()
+		bottom = text_model.make_sentence()
+		return [top, bottom]
+
+t = raw_input("Enter top text keyword: ")
+b = raw_input("Enter bottom text keyword: ")
+res = generateMemeText(t,b)
+print res[0]
+print res[1]
